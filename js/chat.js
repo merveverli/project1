aa2d6da8e1ae60863ff21636c9cdc1551db2d517
merve varli <mervetubaleya@gmail.com>
2025-02-20 15:58:39 +0300

    Add files via upload

---
+++ js/chat.js
@@ -0,0 +1,1 @@
+window.$crisp=[];window.CRISP_WEBSITE_ID="ebfa6748-0e67-430c-9988-dc182058c757";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
